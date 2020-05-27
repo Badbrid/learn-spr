e6634252028142eded9ec33fc62a6466c944707e
zhangsht <Zst@1993>
2020-05-27 18:56:37 +0800

    新增IOC容器，BeanFactory和ApplicationContent

---
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/container/IocContainer.java
@@ -0,0 +1,13 @@
+package org.geekbang.thinking.in.spring.ioc.overview.container;
+
+/**
+ * @author zhangsht
+ * @version 1.0
+ * @date 2020/5/27 18:42
+ */
+public class IocContainer {
+
+    public static void main(String[] args) {
+
+    }
+}
--- thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -6,7 +6,9 @@ import org.geekbang.thinking.in.spring.ioc.overview.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 import java.util.Map;
 
@@ -22,18 +24,23 @@ public class DependencyInjectionDemo {
     public static void main(String[] args) {
         // 配置 XML 配置文件
         // 启动 Spring 应用上下文
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-injection-context.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("META-INF/dependency-injection-context.xml");
         // 依赖来源一：自定义 Bean
-        UserRepository userRepository = beanFactory.getBean("userRepository",UserRepository.class);
+        UserRepository userRepository = applicationContext.getBean("userRepository",UserRepository.class);
 //        System.out.println(userRepository.getUsers());
-//        System.out.println(userRepository.getBeanFactory() == beanFactory);
+        whoIsIocContainer(userRepository,applicationContext);
         // 依赖来源二：依赖注入（內建依赖）
-//        System.out.println(userRepository.getBeanFactory());
+        System.out.println(userRepository.getBeanFactory());
 
         ObjectFactory userFactory = userRepository.getObjectFactory();
 
-        System.out.println(userFactory.getObject() == beanFactory );
+        System.out.println(userFactory.getObject() == applicationContext );
+        //依赖来源三 : 容器内建Bean
+        Environment environment = applicationContext.getBean(Environment.class);
+    }
 
+    private static void whoIsIocContainer(UserRepository userRepository,ApplicationContext applicationContext){
+        System.out.println(userRepository.getBeanFactory() == applicationContext);
     }
 
 
